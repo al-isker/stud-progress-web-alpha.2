19fb5077236a10d56575bc910cc0054701701d01
Alisker <balisker@mail.ru>
2024-07-29 00:59:45 +0300

    header

--- src/components/ordinary/header/Header.tsx
+++ src/components/ordinary/header/Header.tsx
@@ -1,5 +1,21 @@
+'use client';
+
 import { type FC } from 'react';
 
+import { AppBar, Slide, Toolbar, useScrollTrigger } from '@mui/material';
+
 export const Header: FC = () => {
-	return <header>Header</header>;
+	const isScrollDown = useScrollTrigger();
+
+	return (
+		<Slide in={!isScrollDown}>
+			<AppBar position='sticky' elevation={2}>
+				<Toolbar>
+					<strong className='text-2xl font-extrabold text-nowrap'>
+						Stud Progress
+					</strong>
+				</Toolbar>
+			</AppBar>
+		</Slide>
+	);
 };
