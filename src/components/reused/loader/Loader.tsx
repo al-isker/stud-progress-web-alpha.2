bac415999fac62e150762a504463c0e37dcfca2a
Alisker <alisker.babaev@pay-go.ru>
2024-10-20 01:36:32 +0300

    loader

---
+++ src/components/reused/loader/Loader.tsx
@@ -0,0 +1,24 @@
+import style from './loader.module.css';
+
+export const Loader = ({ className }: { className?: string }) => {
+	return (
+		<svg
+			className={`${className ?? ''} ${style.svg}`}
+			display='block'
+			width='100%'
+			height='100%'
+			viewBox='0 0 100 100'
+		>
+			<circle
+				className={`${style.spinner}`}
+				fill='transparent'
+				stroke='currentColor'
+				strokeWidth={8}
+				strokeLinecap='round'
+				cx={50}
+				cy={50}
+				r={45}
+			/>
+		</svg>
+	);
+};
