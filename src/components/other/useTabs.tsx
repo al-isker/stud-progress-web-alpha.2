584050be4f109eecec7c9abac34e07065141e58c
Alisker <alisker.babaev@pay-go.ru>
2024-10-20 01:21:09 +0300

    custom hooks

---
+++ src/components/other/useTabs.tsx
@@ -0,0 +1,49 @@
+import {
+	DonutLarge,
+	HomeRounded,
+	PersonRounded,
+	SchoolRounded
+} from '@mui/icons-material';
+import { Badge } from '@mui/material';
+
+import { ROUTES } from '@/lib/constants/routes';
+
+interface Arguments {
+	isHomeNew?: boolean;
+	isSessionNew?: boolean;
+}
+
+export const useTabs = (badges?: Arguments) => {
+	const { isHomeNew, isSessionNew } = badges ?? {};
+
+	return [
+		{
+			text: 'Главная',
+			href: ROUTES.home,
+			icon: (
+				<Badge color='primary' variant='dot' invisible={!isHomeNew}>
+					<HomeRounded />
+				</Badge>
+			)
+		},
+		{
+			text: 'Сессия',
+			href: ROUTES.session,
+			icon: (
+				<Badge color='primary' variant='dot' invisible={!isSessionNew}>
+					<SchoolRounded />
+				</Badge>
+			)
+		},
+		{
+			text: 'Статистика',
+			href: ROUTES.dashboard,
+			icon: <DonutLarge />
+		},
+		{
+			text: 'Профиль',
+			href: ROUTES.profile,
+			icon: <PersonRounded />
+		}
+	];
+};
