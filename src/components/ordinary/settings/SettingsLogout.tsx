9b284d2cef2c12029eae2adbf7fcc184fc37207d
Alisker <alisker.babaev@pay-go.ru>
2024-10-20 01:40:06 +0300

    setting logout

---
+++ src/components/ordinary/settings/SettingsLogout.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { FC, useState } from 'react';
+
+import { LogoutOutlined } from '@mui/icons-material';
+
+import { MenuItem } from '@/components/reused/menu-item/MenuItem';
+
+import { ROUTES } from '@/lib/constants/routes';
+import { AuthService } from '@/lib/services/authService';
+
+export const SettingsLogout: FC = () => {
+	const [isLoading, setIsLoading] = useState(false);
+
+	const router = useRouter();
+
+	const handleClick = async () => {
+		setIsLoading(true);
+
+		AuthService.logout().then(() => router.replace(ROUTES.login));
+	};
+
+	return (
+		<MenuItem
+			icon={<LogoutOutlined className='rotate-180' />}
+			text='Выйти'
+			onClick={handleClick}
+			loading={isLoading}
+		/>
+	);
+};
