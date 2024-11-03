444dd333258931554ad23239d0f7cf49263c0ace
alisker <balisker@mail.ru>
2024-11-03 16:49:32 +0300

    user name wrap

--- src/components/ordinary/user-name/UserName.tsx
+++ src/components/ordinary/user-name/UserName.tsx
@@ -1,6 +1,6 @@
 export const UserName = ({ name }: { name: string }) => (
 	<div className='px-container-sm'>
-		<h3 className='overflow-hidden text-ellipsis text-nowrap text-center text-[1.35rem] font-semibold leading-[1.85rem] text-black/80'>
+		<h3 className='text-center text-[1.35rem] font-semibold leading-8 text-black/80'>
 			{name}
 		</h3>
 	</div>
